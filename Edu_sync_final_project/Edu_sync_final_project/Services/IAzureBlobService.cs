62d97f0f6fc1435e6110c3653f41733a02051cc5
aksh047 <168117642+aksh047@users.noreply.github.com>
2025-06-06 01:42:06 +0530

    Update IAzureBlobService.cs

--- Edu_sync_final_project/Edu_sync_final_project/Services/IAzureBlobService.cs
+++ Edu_sync_final_project/Edu_sync_final_project/Services/IAzureBlobService.cs
@@ -1,15 +1,1 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 
-namespace Edu_sync_final_project.Services
-{
-    public interface IAzureBlobService
-    {
-        Task<string> UploadFileAsync(IFormFile file, string fileName);
-        Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType);
-        Task DeleteFileAsync(string fileName);
-        Task<string> GetFileUrlAsync(string fileName);
-    }
-} 
